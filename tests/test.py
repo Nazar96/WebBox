60f0fe69578261c165dbcf3d7469eac0a81731e0
nazarii <ntehta96@gmail.com>
2020-06-22 10:22:02 +0300

    updated gitignore

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,16 @@
 from webbox.webbox_generator import WebBoxGenerator
 import unittest
 import os
+import shutil
 
 
 class TestWebBox(unittest.TestCase):
     def setUp(self):
         self.screen_path = './data/image'
         self.markup_path = './data/annot'
+
+        shutil.rmtree(self.screen_path, ignore_errors=True)
+        shutil.rmtree(self.markup_path, ignore_errors=True)
 
         self.wbg = WebBoxGenerator(
             screen_path=self.screen_path,
@@ -17,14 +21,19 @@ class TestWebBox(unittest.TestCase):
         with open('./tests/links.txt', 'r') as f:
             self.links = [line.strip() for line in f.readlines()]
 
+    def tearDown(self):
+        shutil.rmtree(self.screen_path, ignore_errors=True)
+        shutil.rmtree(self.markup_path, ignore_errors=True)
+
     def test_heatmap_generator(self):
         self.wbg.generate(self.links, 'heatmap')
+
         self.assertEqual(
             len(os.listdir(self.screen_path)),
             len(os.listdir(self.markup_path)),
+            "Different number of files in screen and markaup directories"
         )
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
